feffdb127a099490e409c1658f333d74a2edf932
Anton Glushkov <tosha.glushkov@gmail.com>
2024-06-29 13:29:14 +0000

    [bk-1] Подключение общего рест хэндлера

--- profile/src/main/java/com/bank/profile/ProfileApplication.java
+++ profile/src/main/java/com/bank/profile/ProfileApplication.java
@@ -3,9 +3,11 @@ package com.bank.profile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bank.common", "com.bank.profile"})
 public class ProfileApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProfileApplication.class, args);
